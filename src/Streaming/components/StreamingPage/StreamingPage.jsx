520dd676dc6a8b40fb792ce7e0aba5e7b02b077b
javier <javiergda@gmail.com>
2024-08-10 16:43:27 +0200

    creacion useFetch para obtener data

--- src/Streaming/components/StreamingPage/StreamingPage.jsx
+++ src/Streaming/components/StreamingPage/StreamingPage.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import { useFetch } from "../../hooks/useFetch"
+
 
 export const StreamingPage = () => {
+
+
+    const { data, isLoading, hasError } = useFetch('http');
+
+    // {isLoading && <p>Cargando...</p>}
+    // data
+
+
     return (
         <h1>StreamingPage</h1>
     )
---
+++ src/Streaming/hooks/useFetch.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react'
+
+export const useFetch = (url) => {
+
+    const [state, setState] = useState({
+        data: false,
+        isLoading: true,
+        hasError: false,
+        error: false,
+    });
+
+    const getFetch = async () => {
+
+        setState({
+            data: false,
+            isLoading: true,
+            hasError: false,
+            error: false,
+        });
+
+        const resp = await fetch(url);
+
+        if (!resp.ok) {
+            setState({
+                data: false,
+                isLoading: false,
+                hasError: true,
+                error: {
+                    code: resp.status,
+                    message: resp.statusText,
+                }
+            });
+            return;
+        }
+
+        const data = await resp.json();
+
+        setState({
+            data: data,
+            isLoading: false,
+            hasError: false,
+            error: false,
+        });
+    };
+
+    useEffect(() => {
+        getFetch();
+    }, [url]);
+
+    return ({
+        data: state.data,
+        isLoading: state.isLoading,
+        hasError: state.hasError,
+    });
+};
